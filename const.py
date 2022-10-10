ada64ee14f72db515f24706896135d980f51690e
TwoFingers Catering <s0528279@htw-berlin.de>
2022-10-10 15:57:40 +0200

    initial commit

---
+++ const.py
@@ -0,0 +1,13 @@
+pg_userid = 'u528279'
+pg_password= 'p528279'
+pg_host = 'lubuicla.f4.htw-berlin.de'
+pg_db = 'adbkt'
+
+hana_userid='aaa'
+hana_password='aaa'
+hana_host='aaa.f4.htw-berlin.de'
+hana_port='aaa'
+
+neo4j_userid='neo4j'
+neo4j_password='neo4j'
+neo4j_host = "neo4j://localhost:7687"
